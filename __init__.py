425d479665205cc008b13ab3b38a4ff46d2ba2bf
Lezane <clement.lezane@polytechnique.edu>
2019-07-08 19:58:53 +0100

    disable approximation for first version
    disable approximation

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
-# strange bug for all import in __init__.py
-from .approximation import display as approximation_display
+#strange bug for all import in __init__.py and bug with new qiskit and ipyvolume
+#from .approximation import display as approximation_display
 from .colorBar import display as colorbar_display
 from .dial_set import display as dial_set_display
 from .dial_set_3d import display as dial_set_3d_display
 from .pyqt import display as pyqt_display
 
 
-print("approximation_display avaible")
+#print("approximation_display avaible")
 print("colorbar_display avaible")
 print("dial_set_display avaible")
 print("dial_set_3d_display avaible")
